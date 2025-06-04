0a391b0a980d7414f00d98ec826ee143a5214413
AMAN JAIN <142181467+amanjain44@users.noreply.github.com>
2025-06-04 16:31:47 +0000

    ConversionDataTypes

---
+++ 01_basics/03_conversionDatatypes.js
@@ -0,0 +1,35 @@
+// let s = "33";
+// console.log(s);         //33
+// console.log(typeof s);  // string
+
+// let n = Number(s);
+// console.log(n);     //33 as Number
+// console.log(typeof n); // number
+
+
+// let s = "33abs";
+// console.log(s);         //33abs
+// console.log(typeof s);  // string
+
+// let n = Number(s); 
+// console.log(n);     //NaN
+// console.log(typeof n); // number
+
+
+// let s = null;
+// console.log(s);         //null
+// console.log(typeof s);  // object
+
+// let n = Number(s);
+// console.log(n);     //0
+// console.log(typeof n); // number
+
+
+let s = "";
+console.log(s);         //""
+console.log(typeof s);  // string
+
+let n = Boolean(s);
+console.log(n);     //false
+console.log(typeof n); // boolean
+
