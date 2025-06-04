3b3f9d2ccd9fe43683e669c1e37dde2f6d7a9961
AMAN JAIN <142181467+amanjain44@users.noreply.github.com>
2025-06-04 15:32:14 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+// Variables
+// 3 ways to declare a variable in js
+const acc_Id = 10;
+var acc_type = "Saving";
+let email = "aman@gmail.com";
+let name;
+const a=10; //must be initialized 
+console.table([acc_Id,email,acc_type,name,a]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" //use all js code as newer version
+//alert(3+3); //using nodejs not browser
+
+
+let name = "Aman" // string
+let a; // undefined
+console.log(typeof(a));
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 1);
+
